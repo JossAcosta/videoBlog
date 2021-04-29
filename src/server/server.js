638ba8489e8a8958d9e7045c0369d8b341895603
Josseline <josselineacostamtz@gmail.com>
2021-04-29 09:41:41 -0500

    build optimization

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,7 @@ import { StaticRouter } from 'react-router-dom';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
+import getManifest from './getManifest';
 
 dotenv.config();
 
@@ -29,13 +30,19 @@ if(ENV === 'development'){
     app.use(webpackDevMiddleware(compiler, serverConfig));
     app.use(webpackHotMiddleware(compiler));
 }else{
+    app.use( (req, res, next) => {
+        if(!req.hashManifest) req.hashManifest = getManifest();
+        next();
+    });
     app.use(express.static(`${__dirname}/public`));
     app.use(helmet());
     app.use(helmet.permittedCrossDomainPolicies());
     app.disable('x-powered-by');
 }
 
-const setResponse = (html, preloadedState) =>{
+const setResponse = (html, preloadedState, manifest) =>{
+    const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+    const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
     return (`
         <!DOCTYPE html>
             <html lang="en">
@@ -43,7 +50,7 @@ const setResponse = (html, preloadedState) =>{
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="assets/app.css" type"text/css">
+                <link rel="stylesheet" href=${mainStyles} type"text/css">
                 <title>VideoBlog</title>
             </head>
             <body>
@@ -51,7 +58,7 @@ const setResponse = (html, preloadedState) =>{
                 <script>
                     window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
                 </script>
-                <script src="assets/app.js" type="text/javascript"></script>
+                <script src=${mainBuild} type="text/javascript"></script>
             </body>
             </html>
         `
@@ -68,12 +75,12 @@ const renderApp = (req, res, next) => {
             </StaticRouter>
         </Provider>
     );
-        res.send(setResponse(html, preloadedState));
+        res.send(setResponse(html, preloadedState, req.hashManifest));
 }
 
 app.get('*', renderApp);
 
 app.listen(PORT, (err)=>{
     if(err) console.log(err);
-    else console.log('Server running on port 3000');
+    else console.log(`Server running on port ${process.env.PORT}`);
 })
