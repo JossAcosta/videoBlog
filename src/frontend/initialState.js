78bb611c449ca59b44c4ea19a5ab45bd0a451cde
Josseline <josselineacostamtz@gmail.com>
2021-04-28 21:58:23 -0500

    SSR

--- src/frontend/initialState.js
+++ src/frontend/initialState.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider} from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers';
-import App from './routes/App';
-
 const initialState =
 {
         "user":{},
@@ -172,11 +165,4 @@ const initialState =
         ]
 }
 
-const store = createStore(reducer, initialState);
-
-ReactDOM.render(
-    <Provider store = { store }>
-        <App />
-    </Provider>,
-    document.getElementById('app')
-    );
+export default initialState;
